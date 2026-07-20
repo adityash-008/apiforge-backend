3fcb5882273d0b2fdcbf3d366290ac470816d712
Aditya <aadityash8804@gmail.com>
2026-07-20 22:14:52 +0530

    Complete Day 3: Implement POST /projects API

--- src/app.js
+++ src/app.js
@@ -2,37 +2,55 @@ import express from 'express';
 import { projects } from './data/projects.js';
 
 const app = express();
+app.use(express.json());
 
-app.get('/',(req,res)=> {
+//Homepage
+app.get('/', (req, res) => {
     res.send("Welcome to API Forge");
 })
 
-app.get('/projects/:id',(req,res)=> {
+app.get('/projects/:id', (req, res) => {
     const id = parseInt(req.params.id)
     const data = projects.find(project => project.id === id);
-    if(!data){
+    if (!data) {
         return res.status(404).json({
             "success": false,
             "message": "Project Not Found"
         })
     }
     return res.status(200).json({
-    "success": true,
-    "message": "Projects fetched successfully",
-    "data": data
-    
-})
+        "success": true,
+        "message": "Projects fetched successfully",
+        "data": data
+
+    })
 })
 
-app.get('/health',(req,res)=> {
-    res.json({
-        success: true,
-        message: "APIForge server is healthy"
+app.post('/projects', (req, res) => {
+    const projectData = req.body; //Read Data
+
+    if(!projectData.name) return res.status(400).json({message: "Enter Required Field"})
+    if(projectData.name.length<3) return res.status(400).json({message: "Minimum length is 3 "})
+
+    const project = { //Create a object
+        id: projects.length + 1,
+        name: projectData.name,
+        description: projectData.description,
+        category: projectData.category,
+        githubUrl: projectData.githubUrl,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    };
+    //Adding to array
+    projects.push(project);
+    //Sending a response
+    return res.status(201).json({
+        success: true
     })
 })
 
 const PORT = 5000
 
-app.listen(PORT,()=> {
+app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 })
