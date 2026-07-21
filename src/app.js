62a15650122ef5170e89501a9ea9137d8a447438
Aditya <aadityash8804@gmail.com>
2026-07-21 17:27:17 +0530

    Complete Day 4: Implement PUT and DELETE APIs

--- src/app.js
+++ src/app.js
@@ -49,6 +49,41 @@ app.post('/projects', (req, res) => {
     })
 })
 
+app.put('/projects/:id',(req,res) => {
+    const id = parseInt(req.params.id);
+    const updateData = req.body;
+    
+    const project = projects.find(project => project.id === id);
+    if(!project) return res.status(404).json({
+        success:false,
+        message:"Project Not Found!"
+    })
+    Object.assign(project,updateData);
+    project.updatedAt = new Date();
+    return res.status(200).json({
+        success:true,
+        message: "Project updated successfully",
+        data:project
+    })
+})
+
+app.delete('/projects/:id',(req,res)=> {
+    const id = parseInt(req.params.id);
+    const index = projects.findIndex(project => project.id === id);
+
+    if(index === -1) return res.status(404).json({
+        success:false,
+        message:"Project Not Found!"
+    })
+
+    const deletedProject = projects[index];
+    projects.splice(index,1);
+    return res.status(200).json({
+        success:true,
+        message:"Project Deleted Successdully"
+    })
+})
+
 const PORT = 5000
 
 app.listen(PORT, () => {
