5b5ed11f2f6da5cf95db51a23cedfaeeaed427f1
Aditya <aadityash8804@gmail.com>
2026-07-24 23:08:33 +0530

    Secure database credentials

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,12 @@
+
 import mysql from 'mysql2/promise'
 
 const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Aditya#75",
-    database: "apiforge_db"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 })
 
 console.log("✅ MySQL Connected");
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import app from "./app.js";
 import './config/db.js'
+
 
 const PORT = 5000;
 
